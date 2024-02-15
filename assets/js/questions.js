73e12f32f26ab0e9c42a0a959b77f1da28526244
Nami <cenami@live.fr>
2024-02-15 01:12:43 +0000

    added js files and set of questions to question file

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question: "Question 1 : Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "Question 2 : The condition in an if/else statement is within ----:",
+        choices: ["a. quotes", "b. curly brackets", "c. parentheses", "d. square brackets"],
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "Question 3: Arrays in JavaScript can be used to store----:",
+        choices: ["a.numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        correctAnswer: "d"
+    },
+    
+    {
+        question: "Question 4: String values must be enclosed within ----- when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "Question 5 : A very useful tool used during developement and debugging for printing content to the debugger is",
+        choices: ["a. JavaScript", "b. terminal/batch", "c. for loops", "d. console.log"],
+        correctAnswer: "d"
+    }
+    
+    ]
